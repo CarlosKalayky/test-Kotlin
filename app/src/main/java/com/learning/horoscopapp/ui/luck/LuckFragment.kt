301c72b0e6ecddc25ef62b5283f412e3a0db65e2
CZ <carloskalaykytedin@gmail.com>
2024-06-02 11:20:51 +0200

    initial commit

---
+++ app/src/main/java/com/learning/horoscopapp/ui/luck/LuckFragment.kt
@@ -0,0 +1,26 @@
+package com.learning.horoscopapp.ui.luck
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.learning.horoscopapp.R
+import com.learning.horoscopapp.databinding.FragmentHorosBinding
+import com.learning.horoscopapp.databinding.FragmentLuckBinding
+
+class LuckFragment : Fragment(){
+
+    private var _binding: FragmentLuckBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/learning/horoscopapp/ui/palmistry/PalmistryFragment.kt
@@ -0,0 +1,24 @@
+package com.learning.horoscopapp.ui.palmistry
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.learning.horoscopapp.R
+import com.learning.horoscopapp.databinding.FragmentPalmistryBinding
+
+class PalmistryFragment : Fragment() {
+
+    private var _binding:FragmentPalmistryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
